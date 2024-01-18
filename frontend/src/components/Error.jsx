9cd5f1ad070112b5d8df7453915fc7664d182bc4
Rahul Tiwari <rahultiwari161189@gmail.com>
2024-01-18 12:28:11 +0530

    developed error page

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './error.scss';
 
 export default function Error() {
   return (
-    <section>
-      
+    <section className='pageNotFound '>
+    <section className='container'>
+        <section className="errorsectionision">
+          <h1 className='mainHeading'>404-Error</h1>
+          <h2 className='subHeading'>page not found</h2>
+         
+          <section className="backHome">
+            <button className='homeBtn rounded-md bg-white text-black px-3 py-2'>Back To Home</button>
+          </section>
+        </section>
+        <section className="imagesectionision">
+          <section className="layer"></section>
+          <section className="innerImage">
+            {/* <img src={astronaut} alt="astronaut" /> */}
+          </section>
+        </section>
     </section>
+</section>
   )
 }
