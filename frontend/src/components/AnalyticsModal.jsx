0be4d96631c0f150c58493ec71ee1c81bb824774
Rahul Tiwari <rahultiwari161189@gmail.com>
2024-03-15 17:37:18 +0530

    implemented chart downloading feature

--- frontend/src/components/AnalyticsModal.jsx
+++ frontend/src/components/AnalyticsModal.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Modal from './Modal';
 import PieChartComp from './PieChartComp';
-import { ReactComponent as DownloadIcon } from "../assets/Icons/download_icon.svg"
+import { ReactComponent as DownloadIcon } from "../assets/Icons/download_icon.svg";
+import html2canvas from "html2canvas";
+import SubEventChart from './SubEventChart';
+
 export default function AnalyticsModal({ isOpen, close, data }) {
 
+    const chartRef = useRef(null);
+
+    const handleDownload = () => {
+        html2canvas(chartRef.current).then((canvas) => {
+            const url = canvas.toDataURL();
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'reportChart.png';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+    };
 
     return (
         <Modal isOpen={isOpen} close={close} heading={"View Analytics"}>
             <section className="relative w-full h-full border border-blue-500 py-5 px-3">
                 <section className='absolute top-2 right-2 '>
-                    <button className='py-2 px-2 md:py-3 md:px-3  border rounded-lg'>
+                    <button className='py-2 px-2 md:py-3 md:px-3  border rounded-lg'
+                        onClick={handleDownload}
+                    >
                         <DownloadIcon className="h-5 w-5 " />
                     </button>
                 </section>
-                <section className={` my-4  md:flex items-center ${data?.eventData?.hasSubEvents ? "justify-between" : "justify-center"} `}>
+                <section className={` my-4 py-3  md:flex items-center ${data?.eventData?.hasSubEvents ? "justify-between" : "justify-center"} `} ref={chartRef}>
                     <section>
                         <p className="text-xl my-2 text-blue-500 text-center">
                             Overall Participation Analytics
@@ -36,21 +54,7 @@ export default function AnalyticsModal({ isOpen, close, data }) {
                     data?.eventData.hasSubEvents
                         ?
                         data?.eventData?.subEvents?.map((subEvent) => {
-                            return <section key={subEvent.sId} className='relative border my-2 border-blue-500 py-3 px-3 flex items-center justify-center'>
-                                <section className='relative'>
-                                    <section className='absolute top-0 right-4 mx-3 '>
-                                        <button className='py-2 px-2 md:py-3 md:px-3  border rounded-lg'>
-                                            <DownloadIcon className="h-5 w-5 " />
-                                        </button>
-                                    </section>
-                                    <section className="mt-12">
-                                        <p className="text-xl my-2 text-blue-500 text-center">
-                                            Participation Analytics of {subEvent.subEventName}
-                                        </p>
-                                        <PieChartComp data={data} sId={subEvent.sId} courseWise={true} />
-                                    </section>
-                                </section>
-                            </section>
+                            return <SubEventChart data={data} subEvent={subEvent} key={subEvent.sId} />
                         })
                         :
 
